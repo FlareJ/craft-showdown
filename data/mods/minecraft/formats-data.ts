d241586e04c9726f169c1d114051009cc5bc41a3
Flare <joshgarofalo@outlook.com>
2025-04-30 16:26:12 -0400

    initial full commit

---
+++ data/mods/minecraft/formats-data.ts
@@ -0,0 +1,11 @@
+export const FormatsData: import('../sim/dex-species').SpeciesFormatsDataTable = {
+	zombie: {
+		tier: "NFE",
+	},
+	giant: {
+		tier: "OU",
+	},
+	husk: {
+		tier: "OU",
+	},
+};
---
+++ data/mods/minecraft/learnsets.ts
@@ -0,0 +1,33 @@
+export const Learnsets: import('../sim/dex-species').LearnsetDataTable = {
+	zombie: {
+		learnset: {
+			jawlock: ["9M"],
+			crunch: ["9M"],
+			drainpunch: ["9M"],
+			bulkup: ["9M"],
+		},
+		encounters: [
+			{generation: 9, level: 1},
+		],
+	},
+	giant: {
+		learnset: {
+			jawlock: ["9M"],
+			crunch: ["9M"],
+			drainpunch: ["9M"],
+			bulkup: ["9M"],
+			hyperfang: ["9M"],
+			heavyslam: ["9M"],
+		},
+	},
+	husk: {
+		learnset: {
+			jawlock: ["9M"],
+			crunch: ["9M"],
+			drainpunch: ["9M"],
+			bulkup: ["9M"],
+			flareblitz: ["9M"],
+			firefang: ["9M"],
+		},
+	},
+};
