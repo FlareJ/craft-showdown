5bb42ce96f401e3bf5ab6716afeb1807302fc7ad
Flare <joshgarofalo@outlook.com>
2025-05-05 15:08:21 -0400

    Removed minecraft Mod, replaced with gen9.

--- config/custom-formats.ts
+++ config/custom-formats.ts
@@ -5,7 +5,7 @@ export const Formats: FormatList = [
 	{
 		name: "[Gen 9] Minecraft",
 		desc: "Standard singles format with the Minecraft mobs.",
-		mod: 'minecraft',
+		mod: 'gen9',
 		ruleset: ['[Gen 9] Standard AG', '+Craft', '+Craft NFE'],
 		banlist: ['AG', 'OU', 'UU', 'NU', 'RU', 'PU', 'LC', 'NFE'],
 		unbanlist: ['Arceus'],
@@ -13,20 +13,20 @@ export const Formats: FormatList = [
 	{
 		name: "[Gen 9] Minecraft Ubers",
 		desc: "Singles format with the strongest Minecraft mobs.",
-		mod: 'minecraft',
+		mod: 'gen9',
 		ruleset: ['[Gen 9] Minecraft', '+Craft Uber'],
 	},
 	{
 		name: "[Gen 9] Minecraft NFE",
 		desc: "Singles format with only non-fully-evolved Minecraft mobs.",
-		mod: 'minecraft',
+		mod: 'gen9',
 		ruleset: ['[Gen 9] Minecraft', '-Craft'],
 		unbanlist: ['Arceus'],
 	},
 	{
 		name: "[Gen 9] Minecraft Doubles",
 		desc: "Doubles with the Minecraft mobs.",
-		mod: 'minecraft',
+		mod: 'gen9',
 		ruleset: ['[Gen 9] Standard AG', '+Craft', '+Craft NFE'],
 		gameType: 'doubles',
 		banlist: ['AG', 'OU', 'UU', 'NU', 'RU', 'PU', 'LC', 'NFE'],
