b3781e5d9f535f906f76584f93c481040291753a
Flare <joshgarofalo@outlook.com>
2025-05-13 12:55:08 -0400

    Rearranged random battle team data. Added +Past, -Z-Moves, -Berserk Gene to minecraft formats

--- config/custom-formats.ts
+++ config/custom-formats.ts
@@ -4,22 +4,22 @@ export const Formats: FormatList = [
 	},
 	{
 		name: "[Gen 9] Minecraft Random Battle",
-		mod: 'gen9',
-		team: 'minecraftrandom',
-		ruleset: ['Species Clause', 'HP Percentage Mod', 'Cancel Mod'],
+		mod: 'minecraftrandom',
+		team: 'random',
+		ruleset: ['+Past', '-Berserk Gene', 'Species Clause', 'HP Percentage Mod', 'Cancel Mod', "Z-Move Clause"],
 	},
 	{
 		name: "[Gen 9] Minecraft Random Double Battle",
-		mod: 'gen9',
-		team: 'minecraftrandom',
-		ruleset: ['Species Clause', 'HP Percentage Mod', 'Cancel Mod'],
+		mod: 'minecraftrandom',
+		team: 'random',
+		ruleset: ['[Gen 9] Minecraft Random Battle'],
 		gameType: 'doubles',
 	},
 	{
 		name: "[Gen 9] Minecraft",
 		desc: "Standard singles format with the Minecraft mobs.",
 		mod: 'gen9',
-		ruleset: ['Standard AG', '-All Pokemon', '+Craft', '+Craft NFE'],
+		ruleset: ['Standard AG', '-Berserk Gene', '+Past', '-All Pokemon', '+Craft', '+Craft NFE', "Z-Move Clause"],
 	},
 	{
 		name: "[Gen 9] Minecraft Ubers",
@@ -37,7 +37,7 @@ export const Formats: FormatList = [
 		name: "[Gen 9] Minecraft Doubles",
 		desc: "Doubles with the Minecraft mobs.",
 		mod: 'gen9',
-		ruleset: ['Standard AG', '-All Pokemon', '+Craft', '+Craft NFE'],
+		ruleset: ['[Gen 9] Minecraft'],
 		gameType: 'doubles',
 	},
 ];
